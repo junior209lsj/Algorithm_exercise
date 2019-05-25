275bdcb0d6e66bccddc1337a35d1df20ffb57836
root <root@DESKTOP-TUA7VJB.localdomain>
2019-05-25 14:40:42 +0900

    문제풀이

---
+++ B_11723/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+
+struct Instruction
+{
+    int num;
+};
+
+class Solver
+{
+    int m;
+    int flag[21] = { 0 };
+
+};
+
+int main()
+{
+    return 0;
+}
+
